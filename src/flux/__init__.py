26886531fd236739e01e7b7ce07b7ed660019664
profpaul001 <162866316+profpaul001@users.noreply.github.com>
2025-05-23 09:35:59 +0200

    Create __init__.py

---
+++ src/flux/__init__.py
@@ -0,0 +1,13 @@
+"""
+Flux Framework - Il linguaggio per il web moderno
+"""
+
+__version__ = "0.1.0-alpha"
+__author__ = "Paolo Galbiati"
+__email__ = "pg@profpaul.icu"
+__url__ = "https://profpaul.icu/flux/"
+
+# Core exports (sarà implementato durante lo sviluppo)
+# from .core import App
+# from .routing import route
+# from .database import Model
